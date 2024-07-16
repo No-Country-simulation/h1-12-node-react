2252ae01b169f94d5fce68bcbc348aea38cba3ad
Juan Nebbia <juan.nebbia@gmail.com>
2024-07-15 21:09:04 -0300

    Divide register routes to manage role permissions

--- backend/src/middlewares/authentication.middleware.js
+++ backend/src/middlewares/authentication.middleware.js
@@ -2,13 +2,12 @@ import { HttpError } from "../utils/http-error.util.js";
 import { validateAccessToken } from "../utils/jwt.util.js";
 
 export const authenticationMiddleware = (req, res, next) => {
-    const token = req.cookies.token;
-    if (token) {
-        const user = validateAccessToken(token)
-        req.user = user;
-        console.log(user)
-        next();
-    } else {
-        throw new HttpError('Missing credentials')
-    }
-} 
+  const token = req.cookies.token;
+  if (token) {
+    const user = validateAccessToken(token);
+    req.user = user;
+    next();
+  } else {
+    throw new HttpError("Missing credentials");
+  }
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,23 +1,43 @@
-import { Router } from 'express'
-import { AuthController } from '../controllers/auth.controller.js'
-import { authenticationMiddleware } from '../middlewares/authentication.middleware.js'
-import { authorizationMiddleware } from '../middlewares/authorization.middleware.js'
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controller.js";
+import { authenticationMiddleware } from "../middlewares/authentication.middleware.js";
+import { authorizationMiddleware } from "../middlewares/authorization.middleware.js";
 
-const router = Router()
-const authController = new AuthController()
+const router = Router();
+const authController = new AuthController();
 
-router.post('/login', authController.login)
-router.post('/register', authController.register)
+router.post("/login", authController.login);
 
-router.post('/register-patient', 
-    authenticationMiddleware, 
-    authorizationMiddleware(['create-patient']),
-    authController.register)
-router.get('/logout', authController.logout)
+router.post(
+  "/register-patient",
+  authenticationMiddleware,
+  authorizationMiddleware(["create-patient"]),
+  authController.register
+);
 
-router.get('/current', 
-    authenticationMiddleware, 
-    authController.currentUser
-)
+router.post(
+  "/register-professional",
+  authenticationMiddleware,
+  authorizationMiddleware(["create-professional"]),
+  authController.register
+);
 
-export default router
+router.post(
+  "/register-insurance",
+  authenticationMiddleware,
+  authorizationMiddleware(["create-insurance"]),
+  authController.register
+);
+
+router.post(
+  "/register-institution",
+  authenticationMiddleware,
+  authorizationMiddleware(["create-institution"]),
+  authController.register
+);
+
+router.get("/logout", authController.logout);
+
+router.get("/current", authenticationMiddleware, authController.currentUser);
+
+export default router;
