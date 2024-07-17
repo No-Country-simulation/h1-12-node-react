79034f6277eaf89d56445e552b1e215eda583456
Juan Nebbia <juan.nebbia@gmail.com>
2024-07-17 10:12:01 -0300

    Include all routes permissions restrictions

--- backend/src/routes/permissions.routes.js
+++ backend/src/routes/permissions.routes.js
@@ -1,12 +1,32 @@
 import { Router } from 'express'
 import { PermissionsController } from '../controllers/permissions.controller.js'
+import { authenticationMiddleware } from '../middlewares/authentication.middleware.js'
+import { authorizationMiddleware } from '../middlewares/authorization.middleware.js'
 
 const router = Router()
 const permissionsController = new PermissionsController()
 
-router.get('/', permissionsController.getAllPermissions)
-router.get('/:pid', permissionsController.getPermissionById)
-router.post('/', permissionsController.createPermission)
-router.delete('/:pid', permissionsController.deletePermission)
+router.get('/', 
+    authenticationMiddleware,
+    authorizationMiddleware(["get-permissions"]),
+    permissionsController.getAllPermissions
+)
+
+router.get('/:pid', 
+    authenticationMiddleware,
+    authorizationMiddleware(["get-permission"]),
+    permissionsController.getPermissionById
+)
+
+router.post('/', 
+    authenticationMiddleware,
+    authorizationMiddleware(["create-permission"]),
+    permissionsController.createPermission
+)
+
+router.delete('/:pid', 
+    authenticationMiddleware,
+    authorizationMiddleware(["delete-permission"]),
+    permissionsController.deletePermission)
 
 export default router
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,12 +1,22 @@
 import { Router } from 'express'
 import { UsersController } from '../controllers/users.controller.js'
+import { authenticationMiddleware } from '../middlewares/authentication.middleware.js'
+import { authorizationMiddleware } from '../middlewares/authorization.middleware.js'
 
 const router = Router()
 const usersController = new UsersController()
 
-router.get('/', usersController.getAllUsers)
-router.get('/:uid', usersController.getUserById)
-router.post('/', usersController.createUser)
-router.delete('/:uid', usersController.deleteUser)
+router.get('/', 
+    authenticationMiddleware,
+    authorizationMiddleware(["get-users"]),
+    usersController.getAllUsers)
+router.get('/:uid', 
+    authenticationMiddleware,
+    authorizationMiddleware(["get-user"]),
+    usersController.getUserById)
+router.delete('/:uid',
+    authenticationMiddleware,
+    authorizationMiddleware(["delete-user"]),
+    usersController.deleteUser)
 
 export default router
