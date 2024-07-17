6a51c0277d5d7ba3449eaa83977791b1d9ae10e8
Lucas <66272629+MixDevCode@users.noreply.github.com>
2024-07-17 10:07:02 -0300

    agregado cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+//cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 app.use('/api', appRoutes)
 
 app.use(errorMiddleware)
