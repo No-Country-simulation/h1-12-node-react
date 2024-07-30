6b99739b98949e5229b211301cb7e207e2f2e04a
Lucas <66272629+MixDevCode@users.noreply.github.com>
2024-07-29 22:52:41 -0300

    cambio en la url de la api de transcripción

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -11,7 +11,7 @@ export class AiService {
         const form = new FormData();
         form.append('file', fs.createReadStream(req.file.path), req.file.originalname);
 
-        const {data: response } = await axios.post('https://aqua.dcs.codes/s2t', form, {
+        const {data: response } = await axios.post('https://aquamarine.dcs.codes/s2t', form, {
         headers: {
             ...form.getHeaders(),
         },
